c432cf202a0ba080fb817d6be3f489680b891a0f
Daniel Simpson <53801407+babyaced@users.noreply.github.com>
2025-10-28 22:46:45 +0000

    add more examples + cleanup

--- app/HTML/_meta.jsx
+++ app/HTML/_meta.jsx
@@ -1,6 +1,6 @@
 export default {
   index: 'What is HTML?',
   HTMLElementStructure: 'What are HTML Elements?',
-  TheDocumentObjectModel: 'What is the Documnent Object Model?',
+  TheDocumentObjectModel: 'What is the Document Object Model?',
   AdditionalResources: 'Additional Resources',
 }
