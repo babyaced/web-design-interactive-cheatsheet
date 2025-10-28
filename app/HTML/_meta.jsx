3bca46fdb2621c88524da21512b41687594d25c1
Daniel Simpson <53801407+babyaced@users.noreply.github.com>
2025-10-28 19:20:17 +0000

    add more pages/examples

--- app/HTML/_meta.jsx
+++ app/HTML/_meta.jsx
@@ -1,3 +1,6 @@
 export default {
   index: 'What is HTML?',
+  HTMLElementStructure: 'What are HTML Elements?',
+  TheDocumentObjectModel: 'What is the Documnent Object Model?',
+  AdditionalResources: 'Additional Resources',
 }
